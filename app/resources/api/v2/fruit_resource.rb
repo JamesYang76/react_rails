27b2b436050710442475b9b7a2abf47736a08b87
“James <james.yang@rabidtech.co.nz>
2018-09-20 13:22:02 +1200

    Test json api resource

--- app/resources/api/v2/fruit_resource.rb
+++ app/resources/api/v2/fruit_resource.rb
@@ -2,7 +2,14 @@ module Api
   module V2
     class FruitResource < BaseResource
       immutable
-      attributes :name, :description
+      attributes :name, :description, :full_name
+
+      def full_name
+        "full name: #{@model.name}"
+      end
+
+      #filter :name, default: 'Apple'
+
     end
   end
 end
