152e49fd988fe16bc44caf63b2056d0bb55be99c
“James <james.yang@rabidtech.co.nz>
2018-10-04 15:17:46 +1300

    Add async

--- app/javascript/components/fruits/FruitMain.js
+++ app/javascript/components/fruits/FruitMain.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { createStore, applyMiddleware } from 'redux'
-import logger from 'redux-logger'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import rootReducer from './reducers'
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import thunk from 'redux-thunk';
-const middlewares = [ thunk,logger ];
+const middlewares = [ thunk ];
+
+if (process.env.NODE_ENV === `development`) {
+  const { logger } = require(`redux-logger`);
+
+  middlewares.push(logger);
+}
 
 const store = createStore(rootReducer,applyMiddleware( ...middlewares ));
 const basePath = "/fruits";
